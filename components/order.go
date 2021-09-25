8b6d9f9469199f43277346caacfd9a963c4ad3ea
Iana Spivac <yspivac@gmail.com>
2021-09-25 23:23:50 +0300

    restructured the components location

--- components/order.go
+++ components/order.go
@@ -1,8 +1,56 @@
 package components
 
-//type Order struct {
-//	OrderId int
-//	Priority int
-//	MenuItemIds []int
-//	MaxPreparationTime float32
-//}
+import (
+	"dinning-hall/util"
+	"time"
+)
+
+type Order struct {
+	TableId            int     `json:"table_id"`
+	OrderId            int     `json:"order_id"`
+	Priority           int     `json:"priority"`
+	MenuItemIds        []int   `json:"items"`
+	MaxPreparationTime float32 `json:"max_wait"`
+	PickUpTime         int64   `json:"pick_up_time"`
+}
+
+func CreateOrder(tableId int) Order {
+
+	menuItemIds, maxPrepTime := getMenuItems()
+	return Order{
+		TableId:            tableId,
+		OrderId:            util.RandomizeNr(10000),
+		MenuItemIds:        menuItemIds,
+		MaxPreparationTime: maxPrepTime,
+		PickUpTime:         time.Now().Unix(),
+	}
+}
+func findMaxPrepTime(arr []float32) (max float32) {
+	max = arr[0]
+	for _, value := range arr {
+		if value > max {
+			max = value
+		}
+	}
+	return max * 1.3
+}
+func GetMaxPrepTime(foodsId []int, quantityFoods int) float32 {
+	var maxPrepTimeFoods []float32
+	for i := 0; i <= quantityFoods; i++ {
+		maxPrepTimeFoods = append(maxPrepTimeFoods, Menu[foodsId[i]-1].preparationTime)
+	}
+	return findMaxPrepTime(maxPrepTimeFoods)
+}
+
+func getMenuItems() ([]int, float32) {
+	var quantityFoods int = util.RandomizeNr(5)
+	var foodsId []int
+	var maxPrepTime float32
+
+	for i := 0; i <= quantityFoods; i++ {
+		foodsId = append(foodsId, util.RandomizeNr(10))
+	}
+	maxPrepTime = GetMaxPrepTime(foodsId, quantityFoods)
+
+	return foodsId, maxPrepTime
+}
--- components/table.go
+++ components/table.go
@@ -1,34 +1,42 @@
 package components
-//
-//import (
-//	"time"
-//)
-//
-//type Table struct {
-//	TableId int
-//	State State
-//	Order Order
-//}
-//
-//type State int
-//
-//const (
-//	free State = iota
-//	waitingMakeOrder
-//	waitingServeOrder
-//)
-//
-//func GetState(){
-//
-//}
-//
-//func MakeOrder (){
-//for{
-//	time.Sleep(5 * time.Second)
-//
-//}
-//}
-//
-//func WaitingServeOrder(){
-//
-//}
+type Table struct {
+	TableId int
+	State   State
+	Order   Order
+}
+
+type State int
+
+const (
+	free State = iota
+	waitingMakeOrder
+	waitingServeOrder
+)
+
+func InitTable(tableId int) *Table {
+	return &Table{
+		TableId: tableId,
+		State:   free,
+	}
+}
+func GetState() {
+
+}
+
+func WaitingMakeOrder(table *Table) *Table {
+	return &Table{
+		TableId: table.TableId,
+		State:   waitingMakeOrder,
+	}
+}
+func MakeOrder(table *Table) *Table {
+	return &Table{
+		TableId: table.TableId,
+		State:   waitingServeOrder,
+		Order:   CreateOrder(table.TableId),
+	}
+}
+
+func OrderServed(table *Table) *Table {
+	return &Table{}
+}
